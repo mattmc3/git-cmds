83a2f0ebcc71bea2b53e77b2032b02e18571569c
mattmc3 <mattmc3@gmail.com>
2026-02-06 17:04:54 -0500

    Add commands

--- git-cmds.plugin.zsh
+++ git-cmds.plugin.zsh
@@ -4,3 +4,15 @@
 
 0=${(%):-%N}
 path+=(${0:A:h}/bin)
+
+clone() {
+  local dest rc
+  dest="$(git cloner "$@")"
+  rc=$?
+  if (( rc == 150 )); then
+    printf '%s\n' "Already cloned. Taking you there." >&2
+  elif (( rc != 0 )); then
+    return rc
+  fi
+  [[ -d "$dest" ]] && cd "$dest"
+}
